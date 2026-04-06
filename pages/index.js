5b86883433bb13637d5c434e3c96098bfca94bcf
thassio-souza <thassiosouzasilva@gmail.com>
2026-04-06 20:51:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Bora dominar essa parada! &#x1F608; &#x1F608;</h1>
+function Home() {
+  return <h1>Bora dominar essa parada! &#x1F608; &#x1F608;</h1>;
+}
+
+function TextDecoderStream() {
+  console.log("teste");
 }
 
 export default Home;
